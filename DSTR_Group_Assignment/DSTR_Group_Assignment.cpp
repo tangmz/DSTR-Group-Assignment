b7551a676ec8f6f7b20c0e65ce1d30eb80092017
PhanCM <phansilver155@yahoo.com>
2021-11-08 15:02:10 +0800

    Removed testing code in main cpp

--- DSTR_Group_Assignment/DSTR_Group_Assignment.cpp
+++ DSTR_Group_Assignment/DSTR_Group_Assignment.cpp
@@ -12,9 +12,42 @@
 #include "Header Files/ApplicationLists.h"
 using namespace std;
 
+void skipLogin() {
+	int answer = -1;
+	do {
+		system("cls");
+		cout << "1 - Doctor Main Menu\n2 - Nurse Main Menu\n3 - Patient Main Menu\n4 - Exit\n > ";
+		cin >> answer;
+		cin.ignore();
+		switch (answer) {
+		case 1:
+			Interface::DoctorInterface::DisplayMainMenu(ApplicationLists::Patients, ApplicationLists::VisitedPatients, ApplicationLists::CurrentLoginDoctor);
+			break;
+		case 2:
+			Interface::NurseInterface::DisplayMainMenu(ApplicationLists::Patients, ApplicationLists::Users, ApplicationLists::VisitedPatients);
+			break;
+		case 3:
+			Interface::PatientInterface::DisplayMainMenu(ApplicationLists::CurrentLoginPatient, ApplicationLists::Patients, ApplicationLists::Doctors);
+			break;
+		case 4:
+		default:
+			break;
+		}
+	} while (answer != 4);
+}
+
+void addOwnSampleData() {
+	Doctor d1 = Doctor("Doktah", "UserID", "Fname", "lname", 21, 'M', "123", "mail", "addr", "pass", "IC");
+	Nurse n1 = Nurse("Nurse", "UseID", "NFname", "NLname", 1, 'F', "0123", "mail", "addr", "pass", "ic");
+	Patient p1 = Patient("Patient", "UserID", "PFname", "PLname", 2, 'M', "phone", "mail", "addr", "pass", "ic", "illness", "visitD", "visitT");
+	ApplicationLists::Doctors->AddToEnd(d1);
+	ApplicationLists::Nurses->AddToEnd(n1);
+	ApplicationLists::Patients->AddToEnd(p1);
+}
+
 int main() {
-	//Storage for current active users
-	static Doctor* currentDoctor;
+	//Quick access to main menu interfaces
+	//skipLogin();
 
 	srand(unsigned(time(NULL)));
 	AddPatients(ApplicationLists::Users, ApplicationLists::Patients, 150, 18, 21);
@@ -25,19 +58,9 @@ int main() {
 	ApplicationLists::CurrentLoginPatient = ApplicationLists::Patients->GetReference(0);
 	ApplicationLists::CurrentLoginDoctor = ApplicationLists::Doctors->GetReference(0);
 	ApplicationLists::CurrentLoginNurse = ApplicationLists::Nurses->GetReference(0);
-	currentDoctor = ApplicationLists::Doctors->GetReference(0);
-	cout << ApplicationLists::Doctors->GetReference(0)->GetFirstName() << endl;
-	currentDoctor->setIsAvailable(true);
 
 	//ApplicationLists::Medicines->DisplayPages(15);
-
-	DoublyLinkedList<Doctor>* testDoc = new DoublyLinkedList<Doctor>();
-	Doctor d1 = Doctor("Doktah", "UserID", "Fname", "lname", 21, 'M', "123", "mail", "addr", "pass", "IC");
-	Nurse n1 = Nurse("Nurse", "UseID", "NFname", "NLname", 1, 'F', "0123", "mail", "addr", "pass", "ic");
-	Patient p1 = Patient("Patient", "UserID", "PFname", "PLname", 2, 'M', "phone", "mail", "addr", "pass", "ic", "illness", "visitD", "visitT");
-	ApplicationLists::Doctors->AddToEnd(d1);
-	ApplicationLists::Nurses->AddToEnd(n1);
-	ApplicationLists::Patients->AddToEnd(p1);
+	addOwnSampleData();
 
 	string role = "";
 	while (role != "Invalid") {
@@ -63,36 +86,8 @@ int main() {
 		}
 	}
 
-
-	cout << ApplicationLists::CurrentLoginDoctor->GetFirstName() << endl;
-	cout << ApplicationLists::CurrentLoginNurse->GetFirstName() << endl;
-	cout << ApplicationLists::CurrentLoginPatient->GetFirstName() << endl;
-	system("pause");
-	//TEMPORARY: Quick access to main menu interfaces [call skipLogin()]
+	
 	
 	return 0;
 }
 
-void skipLogin() {
-	int answer = -1;
-	do {
-		system("cls");
-		cout << "1 - Doctor Main Menu\n2 - Nurse Main Menu\n3 - Patient Main Menu\n4 - Exit\n > ";
-		cin >> answer;
-		cin.ignore();
-		switch (answer) {
-		case 1:
-			Interface::DoctorInterface::DisplayMainMenu(ApplicationLists::Patients, ApplicationLists::VisitedPatients, ApplicationLists::CurrentLoginDoctor);
-			break;
-		case 2:
-			Interface::NurseInterface::DisplayMainMenu(ApplicationLists::Patients, ApplicationLists::Users, ApplicationLists::VisitedPatients);
-			break;
-		case 3:
-			Interface::PatientInterface::DisplayMainMenu(ApplicationLists::CurrentLoginPatient, ApplicationLists::Patients, ApplicationLists::Doctors);
-			break;
-		case 4:
-		default:
-			break;
-		}
-	} while (answer != 4);
-}
